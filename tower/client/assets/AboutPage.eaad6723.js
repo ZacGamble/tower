fb5f2d0369b2763d21a9dd143321d22dd71599e8
ZacGamble <zacrgamble@gmail.com>
2022-06-09 16:33:00 -0600

    bugfixes

---
+++ tower/client/assets/AboutPage.eaad6723.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.caf4c52d.js";import{o,b as t,d as s}from"./vendor.586f66d3.js";const a={name:"AboutPage"},c={class:"about"},n=s("h1",null,"This is the about page",-1),r=[n];function _(d,i,p,f,u,h){return o(),t("div",c,r)}var b=e(a,[["render",_]]);export{b as default};
